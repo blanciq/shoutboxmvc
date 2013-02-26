749c15489a1653bfc72c83635dd38005f0f1690f
blanciq <pawel.olesiejuk@gmail.com>
2013-02-26 12:56:35 +0100

    Test solution

---
+++ ShoutboxMVC.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using ShoutboxMVC.Controllers;
+
+namespace ShoutboxMVC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+    }
+}
